b5925abc2b5aec5b96625e54cf26879890c3df46
Evan Chen <echen26@students.polytechnic.org>
2024-01-11 09:21:35 -0800

    Added scanner and asked for user inputs.

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,7 +1,16 @@
+import java.util.*;
 public class QuadraticSolver{
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Quadratic Solver.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 'a': ");
+        double a = input.nextDouble();
+        System.out.println("Enter 'b': ");
+        double b = input.nextDouble();
+        System.out.println("Enter 'c': ");
+        double c = input.nextDouble();
+        
     }
 
 }
