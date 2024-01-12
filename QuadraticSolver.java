fc5d772437ee09051056525c656c6ad7442f8850
Evan Chen <echen26@students.polytechnic.org>
2024-01-12 14:34:38 -0800

    added clarifying text

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -3,6 +3,7 @@ public class QuadraticSolver{
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Quadratic Solver.");
+        System.out.println("The Quadratic Formula is: (-b\u00B1sqrt(b^2-4(a)(c)))/(2a)");
         Scanner input = new Scanner(System.in);
         System.out.println("Enter 'a': ");
         double a = input.nextDouble();
@@ -17,6 +18,8 @@ public class QuadraticSolver{
         System.out.println();
         System.out.println("Positive answer = " + posAnswer);
         System.out.println("Negative answer = " + negAnswer);
+        System.out.println();
+        System.out.println("The solver will display 'NaN' if roots are imaginary.");
     }
 
 }
